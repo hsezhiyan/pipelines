69a70f59af0a839e08cace5d6cb95a271e528e0e
Hariharan Sezhiyan <hsezhiyan@ucdavis.edu>
2020-11-11 10:27:49 -0800

    Testing again locally.

--- sdk/python/kfp/compiler/compiler.py
+++ sdk/python/kfp/compiler/compiler.py
@@ -674,7 +674,7 @@ class Compiler(object):
         'templates': templates,
         'arguments': {'parameters': input_params},
         'serviceAccountName': 'pipeline-runner',
-        'ttlSecondsAfterFinished': 0
+        'securityContext': {'fsGroup': 65534},
       }
     }
     # set parallelism limits at pipeline level
